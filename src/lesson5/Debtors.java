339ac3da211cc692bfbb717127e46afb4027fb3a
Dmitriev_Dmitry <37580175+ErlichBachmanRU@users.noreply.github.com>
2021-07-10 00:13:10 +0300

    Update Debtors.java
    Домашка к 5му уроку
    Доделывал уже на рабочем ноуте ,пришлось менять код прямо в гитхабе

--- src/lesson5/Debtors.java
+++ src/lesson5/Debtors.java
@@ -1,6 +1,7 @@
 package lesson5;
 
 public class Debtors {
+
     private String fio;
     private String position;
     private String email;
@@ -8,28 +9,81 @@ public class Debtors {
     private int salary;
     private int age;
 
-    public Debtors(String fio, String position,String email,String fonNumber,int salary,int age) {
+
+    public Debtors(String fio, String position, String email, String fonNumber, int salary, int age) {
         this.fio = fio;
         this.position = position;
         this.email = email;
         this.fonNumber = fonNumber;
         this.salary = salary;
         this.age = age;
+    }
+
+
+    public Debtors() {
+        this("ФИО","должность","Email","foneNumber",10000,18);
 
     }
 
-    public Debtors(String fio) {
-        this.fio = "ФИО";
-        this.position = "должность";
-        this.email = "email";
-        this.fonNumber = "78008008080";
-        this.salary = 10000;
-        this.age = 18;
+    public String getFio() {
+        return fio;
+    }
 
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFonNumber() {
+        return fonNumber;
+    }
+
+    public void setFonNumber(String fonNumber) {
+        this.fonNumber = fonNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void info() {
-        System.out.printf("%s %s %s %s %d %d", fio, position,email,fonNumber,salary,age);
+        System.out.printf("%s %c %s %c %s %c %s %c %d %c %d\n", fio,'|', position,'|', email,'|', fonNumber,'|', salary,'|', age);
+    }
+
+    public void addInfo(String fio, String position, String email, String fonNumber, int salary, int age) {
+        setFio(fio);
+        setPosition(position);
+        setEmail(email);
+        setFonNumber(fonNumber);
+        setSalary(salary);
+        setAge(age);
     }
 
 
