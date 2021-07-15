e7a28172f96d6c2bfc74efd5f917beaff806d028
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-07-15 15:17:40 +0300

    Home work 7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -2,10 +2,17 @@ package lesson7;
 
 public class Main {
     public static void main(String[] args) {
-        Cat barsik = new Cat("Barsik");
-        Plate miska = new Plate(100);
+        Cat[] cats = {
+                new Cat("barsik", false, 15),
+                new Cat("murzik", false, 25)
+        };
+        Plate miska = new Plate(15);
+        for (int i = 0; i < cats.length; i++) {
+            System.out.println(cats[i]);
+            System.out.println(miska);
+            cats[i].eat(miska);
+            System.out.println(cats[i]);
+        }
 
-        barsik.eat(miska);
-        System.out.println(miska);
     }
 }
