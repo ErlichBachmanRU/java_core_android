0e39832ba621acf70143fbc0b6a4a3d77fa410fa
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-07-19 02:37:43 +0300

    Home work 8 Не смог догадаться как сделать прохождение массива участников через массив препятствий

---
+++ src/lesson8/Obstacles.java
@@ -0,0 +1,19 @@
+package lesson8;
+
+public class Obstacles {
+    String name;
+    String type;
+
+    public Obstacles(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ src/lesson8/WallBarrier.java
@@ -0,0 +1,15 @@
+package lesson8;
+
+public class WallBarrier extends Obstacles {
+
+    int height;
+
+    public WallBarrier(String name, String type, int height) {
+        super(name, type);
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
