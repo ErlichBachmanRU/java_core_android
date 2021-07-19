bc60d9e8721aad8c91e2587c81edf5c3a24c47f7
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-07-19 03:03:25 +0300

    Home work 8 Не смог догадаться как сделать прохождение массива участников через массив препятствий

--- src/lesson8/JoggingTrack.java
+++ src/lesson8/JoggingTrack.java
@@ -2,6 +2,12 @@ package lesson8;
 
 public class JoggingTrack extends Obstacles{
 
+    int length;
+
+    public JoggingTrack(String name, String type, int length) {
+        super(name, type);
+        this.length = length;
+    }
 
 
 }
