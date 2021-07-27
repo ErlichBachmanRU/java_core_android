1cceafdcd2d9496ce4a7fff1b4e4792d27e22d12
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-07-27 17:59:39 +0300

    Home work 10

--- src/lesson10/Main.java
+++ src/lesson10/Main.java
@@ -5,5 +5,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+        String[] arr = { "Федотов","Петухов","Ткаченко","Юдин","Федотов","Петухов",
+                "Иванов","Беляев","Потапов","Беляев","Потапов","Зварыч","Бондаренко","Гусев"};
+
+        Set<String> setArr = new HashSet<>();
+        List<String> listArr = new ArrayList<>();
+        Map<String, Integer> countRepeat = new HashMap<>();
+
+        setArr.addAll(Arrays.asList(arr));
+        listArr.addAll(Arrays.asList(arr));
+
+        for (String x : listArr) {
+            if (countRepeat.containsKey(x)) {
+                countRepeat.put(x, countRepeat.get(x) + 1);
+            } else {
+                countRepeat.put(x, 1);
+            }
+        }
+
+        PhoneBook book = new PhoneBook();
+        book.addNew("Petrov","79170000001");
+        book.addNew("Ivanov","79170000002");
+        book.addNew("Sidorov","79170000003");
+
+
+
+
+        System.out.println(listArr);
+        System.out.println(setArr);
+        System.out.println(countRepeat);
+        System.out.println("Petrov:" + book.getNum("Petrov"));
+    }
 
 }
--- src/lesson10/PhoneBook.java
+++ src/lesson10/PhoneBook.java
@@ -4,5 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
+    private Map<String, String> phonebook = new HashMap<>();
 
+    public void addNew(String name ,String tel){
+        phonebook.putIfAbsent(name,tel);
+    }
+
+    public String getNum(String name){
+        return phonebook.get(name);
+    }
 }
