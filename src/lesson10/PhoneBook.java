5b1ec89dadc5da424ecef5632b81f386da20c9f0
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-07-27 17:53:53 +0300

    Home work 10

--- src/lesson10/PhoneBook.java
+++ src/lesson10/PhoneBook.java
@@ -1,4 +1,16 @@
 package lesson10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> phonebook = new HashMap<>();
+
+    public void addNew(String name ,String tel){
+        phonebook.putIfAbsent(name,tel);
+    }
+
+    public String getNum(String name){
+        return phonebook.get(name);
+    }
 }
